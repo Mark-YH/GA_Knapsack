7eccef98250fe27c6f56d469710860c27a128387
Mark Hsu <mark50404@gmail.com>
2019-07-19 16:13:15 +0800

    add multi-processing code

--- Statistic.h
+++ Statistic.h
@@ -2,7 +2,7 @@
 // Created by Mark Hsu on 2019-07-08.
 //
 
-#define ROUND 10 // how many times you wanna execute GA
+#define ROUND 1 // how many times you wanna execute GA
 
 bool isOverweight(int);
 
--- main.cpp
+++ main.cpp
@@ -2,11 +2,28 @@
 #include "Statistic.h"
 #include <iostream>
 #include <time.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main() {
     clock_t tStart = clock(); // count execution time
+
+    pid_t pid = 0;
+
+    for (int i = 0; i < 5; i++) {
+        if (pid == 0) {
+            tStart = clock(); // count execution time
+            pid = fork();
+        } else
+            break;
+    }
+
+    if (pid == 0) {
+        cout << "parent pid: " << pid << endl;
+    } else {
+        cout << "child pid: " << pid << endl;
+    }
 
     // Safety check
 #if K_POINT_CROSSOVER > GENE_LENGTH - 1
@@ -42,6 +59,9 @@ int main() {
     }
     finalResult();
 
+    wait(nullptr);
+    if (pid > 0)
+        exit(0);
     printf("Time taken: %.2fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC); // print execution time
     return 0;
 }
